da4c73ae47836224bbc77b9dfda6fe553ef09479
Darío A. <dario.uribe.bss@gmail.com>
2026-03-02 15:06:16 -0600

    feat(repo): added schema user

--- src/repository/schema.ts
+++ src/repository/schema.ts
@@ -1,20 +1,19 @@
 import { pgTable, primaryKey, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { uuidv7 } from "uuidv7";
 
-export const rolesTable = pgTable("roles", {
+export const rolesTable = pgTable("role", {
     id: uuid().primaryKey().$defaultFn(() => uuidv7()),
     name: text().notNull().unique(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
-
 });
 
 export const policyTable = pgTable("policy", {
     id: uuid().primaryKey().$defaultFn(() => uuidv7()),
     name: text().notNull().unique(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
-})
+});
 
-export const rolePoliciesTable = pgTable("role_policies", {
+export const rolePoliciesTable = pgTable("role_policy", {
     roleId: uuid("role_id").notNull().references(() => rolesTable.id),
     policyId: uuid("policy_id").notNull().references(() => policyTable.id),
 }, (table) => [
@@ -22,3 +21,13 @@ export const rolePoliciesTable = pgTable("role_policies", {
         columns: [table.roleId, table.policyId]
     })
 ]);
+
+export const user = pgTable("user", {
+    id: uuid().primaryKey().$defaultFn(() => uuidv7()),
+    firstName: text(),
+    lastName: text(),
+    phone: text().notNull().unique(),
+    email: text().notNull().unique(),
+    roleId: uuid().references(() => rolesTable.id),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+})
