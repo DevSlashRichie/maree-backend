22fcbda32e68c00a71c22260958bcc8a679fbb4a
D1ego <diegommor@gmail.com>
2026-03-02 14:31:20 -0600

    Policy table

--- src/repository/schema.ts
+++ src/repository/schema.ts
@@ -3,16 +3,20 @@ import { uuidv7 } from "uuidv7";
 
 export const rolesTable = pgTable("roles", {
     id: uuid().primaryKey().$defaultFn(() => uuidv7()),
-    name: text().notNull(),
+    name: text().notNull().unique(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
 
 });
 
-
+export const policyTable = pgTable("policy", {
+    id: uuid().primaryKey().$defaultFn(() => uuidv7()),
+    name: text().notNull().unique(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+})
 
 export const rolePoliciesTable = pgTable("role_policies", {
     roleId: uuid("role_id").notNull().references(() => rolesTable.id),
-    policyId: uuid("policy_id")
+    policyId: uuid("policy_id").notNull().references(() => policyTable.id),
 }, (table) => [
     primaryKey({
         columns: [table.roleId, table.policyId]
