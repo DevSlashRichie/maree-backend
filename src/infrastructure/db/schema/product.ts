20543b90f4664407a26551a294595c0782120c62
alexxbez <alexxbez@proton.me>
2026-03-03 11:55:19 -0600

    feat(infrastructure): add categosy and product tables

--- src/infrastructure/db/schema/product.ts
+++ src/infrastructure/db/schema/product.ts
@@ -1,5 +1,28 @@
-import { bigint, pgTable, text, uuid } from "drizzle-orm/pg-core";
+import type { AnyPgColumn } from "drizzle-orm/pg-core";
+import { bigint, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { uuidv7 } from "uuidv7";
+
+export const categoryTable = pgTable("category", {
+  id: uuid()
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  name: text().notNull(),
+  description: text(),
+  parentId: uuid("parent_id").references((): AnyPgColumn => categoryTable.id),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productTable = pgTable("product", {
+  id: uuid()
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  name: text().notNull(),
+  status: text().notNull(),
+  categoryId: uuid()
+    .notNull()
+    .references(() => categoryTable.id),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
 
 export const productVariantsTable = pgTable("product_variant", {
   id: uuid()
@@ -8,6 +31,7 @@ export const productVariantsTable = pgTable("product_variant", {
   name: text().notNull(),
   price: bigint({ mode: "bigint" }).notNull(),
   image: text(),
-  productId: uuid().notNull(),
-  //.references(() => productsTable.id),
+  productId: uuid()
+    .notNull()
+    .references(() => productTable.id),
 });
