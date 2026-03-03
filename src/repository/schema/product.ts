7aa73e70341aac3aa9129dd83510dc3aa654739d
D1ego <diegommor@gmail.com>
2026-03-02 21:17:53 -0600

    feat: add 7 middle schemas

---
+++ src/repository/schema/product.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, timestamp, uuid, bigint } from "drizzle-orm/pg-core";
+import { uuidv7 } from "uuidv7";
+
+export const productVariantsTable = pgTable("product_variant", {
+    id: uuid().primaryKey().$defaultFn(() => uuidv7()),
+    name: text().notNull(),
+    price: bigint({ mode: "bigint" }).notNull(),
+    image: text(),
+    productId: uuid().notNull().references(() => products.id),
+});
