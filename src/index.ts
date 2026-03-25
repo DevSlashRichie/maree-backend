f77f6024c5c88c40977ed3c11775cb0b0961a71c
DevSlashRichie <ricardo@binarysails.com>
2026-03-25 14:48:30 -0600

    chore: biome fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { execSync } from "child_process";
+import { execSync } from "node:child_process";
 import { createHttpServer, EnvHttpConf } from "./http";
 import "dotenv/config";
 import { logger } from "@/lib/logger";
