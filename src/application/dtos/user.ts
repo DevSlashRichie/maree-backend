f50133b67443c5a73dcb6e3402d20077957bf6e6
DevSlashRichie <ricardo@binarysails.com>
2026-04-01 17:38:51 -0600

    fix: StaffList dto was incorrect

--- src/application/dtos/user.ts
+++ src/application/dtos/user.ts
@@ -1,4 +1,5 @@
 import { z } from "@hono/zod-openapi";
+import { ActorSchema } from "@/domain/entities/actor";
 import { UserSchema } from "@/domain/entities/user";
 
 export const UserWithStatsSchema = UserSchema.extend({
@@ -17,7 +18,7 @@ export const UserListSchema = z
 
 export const StaffListSchema = z
   .object({
-    users: z.array(UserSchema),
+    users: z.array(ActorSchema),
     total: z.number(),
     page: z.number(),
     limit: z.number(),
