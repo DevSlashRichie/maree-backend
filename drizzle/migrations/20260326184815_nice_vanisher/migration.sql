6594fb90962e038c0396d98b1eec1140e11797c8
DevSlashRichie <ricardo@binarysails.com>
2026-04-05 13:41:57 -0600

    ci: migrations conflict solved

--- drizzle/migrations/20260326184815_nice_vanisher/migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "loyalty_transaction" DROP CONSTRAINT "loyalty_transaction_loyalty_card_id_loyalty_card_id_fkey";--> statement-breakpoint
-DROP TABLE "loyalty_card";--> statement-breakpoint
-ALTER TABLE "loyalty_transaction" ADD COLUMN "user_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "loyalty_transaction" DROP COLUMN "loyalty_card_id";--> statement-breakpoint
-ALTER TABLE "loyalty_transaction" ADD CONSTRAINT "loyalty_transaction_user_id_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id");
